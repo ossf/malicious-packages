aba21365bb409a4da200b14385bd6569b2b497c2
Caleb Brown <calebbrown@google.com>
2025-09-02 17:03:34 +1000

    Fix data for copyright header
    Signed-off-by: Caleb Brown <calebbrown@google.com>

--- cmd/stats/main.go
+++ cmd/stats/main.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Malicious Packages Authors
+// Copyright 2025 Malicious Packages Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
