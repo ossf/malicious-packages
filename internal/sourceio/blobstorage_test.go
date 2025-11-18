9b145c39a612325321b03cdb5683368e14ec794b
Caleb Brown <calebbrown@google.com>
2025-11-18 12:11:56 +1100

    Stop goconst lint race failure.
    Signed-off-by: Caleb Brown <calebbrown@google.com>

--- internal/sourceio/blobstorage_test.go
+++ internal/sourceio/blobstorage_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//nolint:goconst
+//nolint:goconst,nolintlint
 package sourceio_test
 
 import (
