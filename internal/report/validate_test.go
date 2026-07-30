e73b4d2032ce54d2abcd61ed2459d3a8e74ba74a
Caleb Brown <calebbrown@google.com>
2026-07-30 15:28:18 +1000

    Update osvschema to use the protobuf Vulnerability structure. (#1409)
    * Update osvschema to use proto based bindings.
    
    Signed-off-by: Caleb Brown <calebbrown@google.com>
    
    * Fix whitespace. Add a case to handle empty origins.
    
    Signed-off-by: Caleb Brown <calebbrown@google.com>
    
    * Minor tweaks to try and fix failed checks.
    
    Signed-off-by: Caleb Brown <calebbrown@google.com>
    
    * Fix broken ecosystem specific validation.
    
    Signed-off-by: Caleb Brown <calebbrown@google.com>
    
    * Fix broken validate tests.
    
    Signed-off-by: Caleb Brown <calebbrown@google.com>
    
    * Use go-cmp and protocmp in merge_tests for simplicity.
    
    Signed-off-by: Caleb Brown <calebbrown@google.com>
    
    * Fix whitespace linter issues.
    
    Signed-off-by: Caleb Brown <calebbrown@google.com>
    
    ---------
    
    Signed-off-by: Caleb Brown <calebbrown@google.com>

--- internal/report/validate_test.go
+++ internal/report/validate_test.go
@@ -3,31 +3,34 @@ package report_test
 import (
 	"testing"
 
+	"github.com/ossf/osv-schema/bindings/go/osvconstants"
 	"github.com/ossf/osv-schema/bindings/go/osvschema"
+	"google.golang.org/protobuf/types/known/structpb"
 
 	"github.com/ossf/malicious-packages/internal/report"
 )
 
 func TestValidateVuln_Valid(t *testing.T) {
+	emptyStruct, _ := structpb.NewStruct(make(map[string]any))
 	vuln := &osvschema.Vulnerability{
-		Affected: []osvschema.Affected{
+		Affected: []*osvschema.Affected{
 			{
-				Package: osvschema.Package{
-					Ecosystem: string(osvschema.EcosystemDebian) + ":7",
+				Package: &osvschema.Package{
+					Ecosystem: string(osvconstants.EcosystemDebian) + ":7",
 					Name:      "example",
 					Purl:      "pkg:deb/debian/example",
 				},
-				Ranges: []osvschema.Range{
+				Ranges: []*osvschema.Range{
 					{
-						Type: osvschema.RangeEcosystem,
-						Events: []osvschema.Event{
+						Type: osvschema.Range_ECOSYSTEM,
+						Events: []*osvschema.Event{
 							{Introduced: "0"},
 							{Fixed: "1"},
 						},
 					},
 				},
 				Versions:          []string{"0", "0.1"},
-				EcosystemSpecific: make(map[string]any),
+				EcosystemSpecific: emptyStruct,
 			},
 		},
 	}
@@ -39,16 +42,16 @@ func TestValidateVuln_Valid(t *testing.T) {
 
 func TestValidateVuln_Valid_SemVer(t *testing.T) {
 	vuln := &osvschema.Vulnerability{
-		Affected: []osvschema.Affected{
+		Affected: []*osvschema.Affected{
 			{
-				Package: osvschema.Package{
-					Ecosystem: string(osvschema.EcosystemCratesIO),
+				Package: &osvschema.Package{
+					Ecosystem: string(osvconstants.EcosystemCratesIO),
 					Name:      "example",
 				},
-				Ranges: []osvschema.Range{
+				Ranges: []*osvschema.Range{
 					{
-						Type: osvschema.RangeSemVer,
-						Events: []osvschema.Event{
+						Type: osvschema.Range_SEMVER,
+						Events: []*osvschema.Event{
 							{Introduced: "0"},
 							{Fixed: "1.0.0"},
 						},
@@ -65,21 +68,21 @@ func TestValidateVuln_Valid_SemVer(t *testing.T) {
 
 func TestValidateVuln_Valid_Git(t *testing.T) {
 	vuln := &osvschema.Vulnerability{
-		Affected: []osvschema.Affected{
+		Affected: []*osvschema.Affected{
 			{
 				Versions: []string{"0.1.0"},
-				Ranges: []osvschema.Range{
+				Ranges: []*osvschema.Range{
 					{
-						Type: osvschema.RangeGit,
-						Events: []osvschema.Event{
+						Type: osvschema.Range_GIT,
+						Events: []*osvschema.Event{
 							{Introduced: "0"},
 							{Fixed: "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"},
 						},
 						Repo: "https://example.org/repo.git",
 					},
 					{
-						Type: osvschema.RangeGit,
-						Events: []osvschema.Event{
+						Type: osvschema.Range_GIT,
+						Events: []*osvschema.Event{
 							{Introduced: "0"},
 							{Fixed: "da39a3ee5e6b4b0d3255bfef95601890afd80709"},
 						},
@@ -106,16 +109,16 @@ func TestValidateVuln_Fail_NoAffected(t *testing.T) {
 
 func TestValidateVuln_Fail_TwoAffected(t *testing.T) {
 	vuln := &osvschema.Vulnerability{
-		Affected: []osvschema.Affected{
+		Affected: []*osvschema.Affected{
 			{
-				Package: osvschema.Package{
-					Ecosystem: string(osvschema.EcosystemPyPI),
+				Package: &osvschema.Package{
+					Ecosystem: string(osvconstants.EcosystemPyPI),
 					Name:      "example1",
 				},
 			},
 			{
-				Package: osvschema.Package{
-					Ecosystem: string(osvschema.EcosystemPyPI),
+				Package: &osvschema.Package{
+					Ecosystem: string(osvconstants.EcosystemPyPI),
 					Name:      "example2",
 				},
 			},
@@ -130,7 +133,7 @@ func TestValidateVuln_Fail_TwoAffected(t *testing.T) {
 
 func TestValidateVuln_Fail_NoPackage(t *testing.T) {
 	vuln := &osvschema.Vulnerability{
-		Affected: []osvschema.Affected{
+		Affected: []*osvschema.Affected{
 			{
 				Versions: []string{"0"},
 			},
@@ -145,10 +148,10 @@ func TestValidateVuln_Fail_NoPackage(t *testing.T) {
 
 func TestValidateVuln_Fail_NoPackageName(t *testing.T) {
 	vuln := &osvschema.Vulnerability{
-		Affected: []osvschema.Affected{
+		Affected: []*osvschema.Affected{
 			{
-				Package: osvschema.Package{
-					Ecosystem: string(osvschema.EcosystemNPM),
+				Package: &osvschema.Package{
+					Ecosystem: string(osvconstants.EcosystemNPM),
 				},
 				Versions: []string{"0"},
 			},
@@ -163,10 +166,10 @@ func TestValidateVuln_Fail_NoPackageName(t *testing.T) {
 
 func TestValidateVuln_Fail_PackageNameSpecialChars(t *testing.T) {
 	vuln := &osvschema.Vulnerability{
-		Affected: []osvschema.Affected{
+		Affected: []*osvschema.Affected{
 			{
-				Package: osvschema.Package{
-					Ecosystem: string(osvschema.EcosystemNPM),
+				Package: &osvschema.Package{
+					Ecosystem: string(osvconstants.EcosystemNPM),
 					Name:      "exam\rple",
 				},
 				Versions: []string{"0"},
@@ -182,9 +185,9 @@ func TestValidateVuln_Fail_PackageNameSpecialChars(t *testing.T) {
 
 func TestValidateVuln_Fail_NoEcosystem(t *testing.T) {
 	vuln := &osvschema.Vulnerability{
-		Affected: []osvschema.Affected{
+		Affected: []*osvschema.Affected{
 			{
-				Package: osvschema.Package{
+				Package: &osvschema.Package{
 					Name: "example",
 				},
 				Versions: []string{"0"},
@@ -200,9 +203,9 @@ func TestValidateVuln_Fail_NoEcosystem(t *testing.T) {
 
 func TestValidateVuln_Fail_InvalidEcosystem(t *testing.T) {
 	vuln := &osvschema.Vulnerability{
-		Affected: []osvschema.Affected{
+		Affected: []*osvschema.Affected{
 			{
-				Package: osvschema.Package{
+				Package: &osvschema.Package{
 					Ecosystem: "pypi",
 					Name:      "example",
 				},
@@ -239,73 +242,79 @@ func TestValidateVuln_Fail_InvalidRange(t *testing.T) {
 	tests := []struct {
 		name  string
 		isGit bool
-		r     osvschema.Range
+		r     *osvschema.Range
 	}{
 		{
 			name: "empty",
 		},
 		{
+			name: "unspecified type",
+			r: &osvschema.Range{
+				Type: osvschema.Range_UNSPECIFIED,
+			},
+		},
+		{
 			name: "invalid type",
-			r: osvschema.Range{
-				Type: "invalid",
+			r: &osvschema.Range{
+				Type: -1,
 			},
 		},
 		{
 			name: "invalid semver type",
-			r: osvschema.Range{
-				Type: osvschema.RangeSemVer,
-				Events: []osvschema.Event{
+			r: &osvschema.Range{
+				Type: osvschema.Range_SEMVER,
+				Events: []*osvschema.Event{
 					{Introduced: "0"},
 				},
 			},
 		},
 		{
 			name: "no events",
-			r: osvschema.Range{
-				Type: osvschema.RangeEcosystem,
+			r: &osvschema.Range{
+				Type: osvschema.Range_ECOSYSTEM,
 			},
 		},
 		{
 			name: "empty event",
-			r: osvschema.Range{
-				Type: osvschema.RangeEcosystem,
-				Events: []osvschema.Event{
+			r: &osvschema.Range{
+				Type: osvschema.Range_ECOSYSTEM,
+				Events: []*osvschema.Event{
 					{},
 				},
 			},
 		},
 		{
 			name: "invalid event 1",
-			r: osvschema.Range{
-				Type: osvschema.RangeEcosystem,
-				Events: []osvschema.Event{
+			r: &osvschema.Range{
+				Type: osvschema.Range_ECOSYSTEM,
+				Events: []*osvschema.Event{
 					{Introduced: "0", Fixed: "1"},
 				},
 			},
 		},
 		{
 			name: "invalid event 2",
-			r: osvschema.Range{
-				Type: osvschema.RangeEcosystem,
-				Events: []osvschema.Event{
+			r: &osvschema.Range{
+				Type: osvschema.Range_ECOSYSTEM,
+				Events: []*osvschema.Event{
 					{Introduced: "0", LastAffected: "1"},
 				},
 			},
 		},
 		{
 			name: "invalid event 3",
-			r: osvschema.Range{
-				Type: osvschema.RangeGit,
-				Events: []osvschema.Event{
+			r: &osvschema.Range{
+				Type: osvschema.Range_GIT,
+				Events: []*osvschema.Event{
 					{Introduced: "0", Limit: "1"},
 				},
 			},
 		},
 		{
 			name: "fixed and last affected",
-			r: osvschema.Range{
-				Type: osvschema.RangeEcosystem,
-				Events: []osvschema.Event{
+			r: &osvschema.Range{
+				Type: osvschema.Range_ECOSYSTEM,
+				Events: []*osvschema.Event{
 					{Introduced: "0"},
 					{LastAffected: "1"},
 					{Fixed: "2"},
@@ -314,49 +323,49 @@ func TestValidateVuln_Fail_InvalidRange(t *testing.T) {
 		},
 		{
 			name: "git no repo",
-			r: osvschema.Range{
-				Type: osvschema.RangeGit,
-				Events: []osvschema.Event{
+			r: &osvschema.Range{
+				Type: osvschema.Range_GIT,
+				Events: []*osvschema.Event{
 					{Introduced: "0"},
 				},
 			},
 		},
 		{
 			name: "git invalid repo",
-			r: osvschema.Range{
-				Type: osvschema.RangeGit,
+			r: &osvschema.Range{
+				Type: osvschema.Range_GIT,
 				Repo: ":",
-				Events: []osvschema.Event{
+				Events: []*osvschema.Event{
 					{Introduced: "0"},
 				},
 			},
 		},
 		{
 			name: "git non-hex commit id",
-			r: osvschema.Range{
-				Type: osvschema.RangeGit,
+			r: &osvschema.Range{
+				Type: osvschema.Range_GIT,
 				Repo: "https://example.com/repo.git",
-				Events: []osvschema.Event{
+				Events: []*osvschema.Event{
 					{Introduced: "this is not hex"},
 				},
 			},
 		},
 		{
 			name: "git non-sha commit id",
-			r: osvschema.Range{
-				Type: osvschema.RangeGit,
+			r: &osvschema.Range{
+				Type: osvschema.Range_GIT,
 				Repo: "https://example.com/repo.git",
-				Events: []osvschema.Event{
+				Events: []*osvschema.Event{
 					{Introduced: "deadbeef"},
 				},
 			},
 		},
 		{
 			name: "git zero commit",
-			r: osvschema.Range{
-				Type: osvschema.RangeGit,
+			r: &osvschema.Range{
+				Type: osvschema.Range_GIT,
 				Repo: "https://example.com/repo.git",
-				Events: []osvschema.Event{
+				Events: []*osvschema.Event{
 					{Introduced: "0"},
 					{Fixed: "0"},
 				},
@@ -365,10 +374,10 @@ func TestValidateVuln_Fail_InvalidRange(t *testing.T) {
 		{
 			name:  "git non-git type",
 			isGit: true,
-			r: osvschema.Range{
-				Type: osvschema.RangeEcosystem,
+			r: &osvschema.Range{
+				Type: osvschema.Range_ECOSYSTEM,
 				Repo: "https://example.com/repo.git",
-				Events: []osvschema.Event{
+				Events: []*osvschema.Event{
 					{Introduced: "0"},
 				},
 			},
@@ -376,16 +385,18 @@ func TestValidateVuln_Fail_InvalidRange(t *testing.T) {
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			var pkg osvschema.Package
+			var pkg *osvschema.Package
 			if !test.isGit {
-				pkg.Ecosystem = string(osvschema.EcosystemPyPI)
-				pkg.Name = "example"
+				pkg = &osvschema.Package{
+					Ecosystem: string(osvconstants.EcosystemPyPI),
+					Name:      "example",
+				}
 			}
 			vuln := &osvschema.Vulnerability{
-				Affected: []osvschema.Affected{
+				Affected: []*osvschema.Affected{
 					{
 						Package: pkg,
-						Ranges:  []osvschema.Range{test.r},
+						Ranges:  []*osvschema.Range{test.r},
 					},
 				},
 			}
@@ -399,19 +410,19 @@ func TestValidateVuln_Fail_InvalidRange(t *testing.T) {
 
 func TestValidateVuln_Fail_DifferentRepos(t *testing.T) {
 	vuln := &osvschema.Vulnerability{
-		Affected: []osvschema.Affected{
+		Affected: []*osvschema.Affected{
 			{
-				Ranges: []osvschema.Range{
+				Ranges: []*osvschema.Range{
 					{
-						Type: osvschema.RangeGit,
-						Events: []osvschema.Event{
+						Type: osvschema.Range_GIT,
+						Events: []*osvschema.Event{
 							{Introduced: "0"},
 						},
 						Repo: "https://example.org/first.git",
 					},
 					{
-						Type: osvschema.RangeGit,
-						Events: []osvschema.Event{
+						Type: osvschema.Range_GIT,
+						Events: []*osvschema.Event{
 							{Introduced: "0"},
 						},
 						Repo: "https://example.org/second.git",
@@ -429,60 +440,60 @@ func TestValidateVuln_Fail_DifferentRepos(t *testing.T) {
 func TestValidateVuln_Fail_InvalidPURLs(t *testing.T) {
 	tests := []struct {
 		name string
-		p    osvschema.Package
+		p    *osvschema.Package
 	}{
 		{
 			name: "purl parse error",
-			p: osvschema.Package{
-				Ecosystem: string(osvschema.EcosystemNPM),
+			p: &osvschema.Package{
+				Ecosystem: string(osvconstants.EcosystemNPM),
 				Name:      "example",
 				Purl:      "not_a_purl",
 			},
 		},
 		{
 			name: "ecosystem mismatch 1",
-			p: osvschema.Package{
-				Ecosystem: string(osvschema.EcosystemNPM),
+			p: &osvschema.Package{
+				Ecosystem: string(osvconstants.EcosystemNPM),
 				Name:      "example",
 				Purl:      "pkg:pypi/example",
 			},
 		},
 		{
 			name: "ecosystem mismatch 2",
-			p: osvschema.Package{
-				Ecosystem: string(osvschema.EcosystemNPM),
+			p: &osvschema.Package{
+				Ecosystem: string(osvconstants.EcosystemNPM),
 				Name:      "example",
 				Purl:      "pkg:oci/example",
 			},
 		},
 		{
 			name: "name mismatch",
-			p: osvschema.Package{
-				Ecosystem: string(osvschema.EcosystemNPM),
+			p: &osvschema.Package{
+				Ecosystem: string(osvconstants.EcosystemNPM),
 				Name:      "example1",
 				Purl:      "pkg:npm/example2",
 			},
 		},
 		{
 			name: "namespace mismatch 1",
-			p: osvschema.Package{
-				Ecosystem: string(osvschema.EcosystemNPM),
+			p: &osvschema.Package{
+				Ecosystem: string(osvconstants.EcosystemNPM),
 				Name:      "@org/example",
 				Purl:      "pkg:npm/example",
 			},
 		},
 		{
 			name: "namespace mismatch 2",
-			p: osvschema.Package{
-				Ecosystem: string(osvschema.EcosystemNPM),
+			p: &osvschema.Package{
+				Ecosystem: string(osvconstants.EcosystemNPM),
 				Name:      "example",
 				Purl:      "pkg:npm/%40org/example",
 			},
 		},
 		{
-			name: "namespace mismatch 2",
-			p: osvschema.Package{
-				Ecosystem: string(osvschema.EcosystemDebian) + ":7",
+			name: "namespace mismatch 3",
+			p: &osvschema.Package{
+				Ecosystem: string(osvconstants.EcosystemDebian) + ":7",
 				Name:      "example",
 				Purl:      "pkg:deb/notdebian/example",
 			},
@@ -491,7 +502,7 @@ func TestValidateVuln_Fail_InvalidPURLs(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			vuln := &osvschema.Vulnerability{
-				Affected: []osvschema.Affected{
+				Affected: []*osvschema.Affected{
 					{
 						Package:  test.p,
 						Versions: []string{"0"},
@@ -507,16 +518,18 @@ func TestValidateVuln_Fail_InvalidPURLs(t *testing.T) {
 }
 
 func TestValidateVuln_Fail_EcosystemSpecific(t *testing.T) {
+	ecosystemSpecific, _ := structpb.NewStruct(
+		map[string]any{
+			"test": "not empty",
+		})
 	vuln := &osvschema.Vulnerability{
-		Affected: []osvschema.Affected{
+		Affected: []*osvschema.Affected{
 			{
-				Package: osvschema.Package{
-					Ecosystem: string(osvschema.EcosystemPyPI),
+				Package: &osvschema.Package{
+					Ecosystem: string(osvconstants.EcosystemPyPI),
 					Name:      "example",
 				},
-				EcosystemSpecific: map[string]any{
-					"test": "not empty",
-				},
+				EcosystemSpecific: ecosystemSpecific,
 			},
 		},
 	}
